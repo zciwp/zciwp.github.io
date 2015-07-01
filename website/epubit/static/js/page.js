1038b1b2a8081a4078eb50a0f287031e995cf2d4
zciwp <chao.zhang.bj@gmail.com>
2015-07-01 22:54:33 +0800

    update

--- website/epubit/static/js/page.js
+++ website/epubit/static/js/page.js
@@ -1,44 +1,3 @@
- // ie实现placeholder
-jQuery.fn.placeholder = function(){
-    var i = document.createElement('input'),
-        placeholdersupport = 'placeholder' in i;
-    if(!placeholdersupport){
-        var inputs = jQuery(this);
-        inputs.each(function(){
-            var input = jQuery(this),
-                text = input.attr('placeholder'),
-                pdl = 0,
-                height = input.outerHeight(),
-                width = input.outerWidth(),
-                placeholder = jQuery('<span class="phTips">'+text+'</span>');
-            try{
-                pdl = input.css('padding-left').match(/\d*/i)[0] * 1;
-            }catch(e){
-                pdl = 5;
-            }
-            placeholder.css({'margin-left': -(width-pdl),'height':height,'line-height':height+"px",'position':'absolute', 'color': "#999", 'font-size' : "12px"});
-            placeholder.click(function(){
-                input.focus();
-            });
-            if(input.val() != ""){
-                placeholder.css({display:'none'});
-            }else{
-                placeholder.css({display:'inline'});
-            }
-            placeholder.insertAfter(input);
-            input.keyup(function(e){
-                if(jQuery(this).val() != ""){
-                    placeholder.css({display:'none'});
-                }else{
-                    placeholder.css({display:'inline'});
-                }
-            });
-        });
-    }
-    return this;
-};
-jQuery('input[placeholder]').placeholder();
-
 $(function(e){
     if (navigator.userAgent.match(/mobile/i)) {
         $('.dropdown-menu .hasSub>a').bind('touchstart',function(e){
@@ -47,6 +6,12 @@ $(function(e){
             $(this).parent().siblings().find('.sub-menu2').hide();
             $(this).next('.sub-menu2').toggle();
         });
+    }
+    var UA = navigator.userAgent,
+    start = navigator.userAgent.indexOf('MSIE')+4,
+    ver = UA.substr(start,2);
+    if(ver < 8){
+        window.location.href = "warnpage.html";
     }
     //图书分类
     $('.maintab a').bind('click',function(){
